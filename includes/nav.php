05e9cec7bf40e7bcce39c6326f265900310113c4
abbasi303 <usalcgg18@gmail.com>
2023-04-14 09:40:45 +0200

    first commit

---
+++ includes/nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar">
+    <div class="link-container">
+        <a href="./">
+            <img class="nav-logo" src="/../assets/nav-logo.png" />
+        </a>
+        <div class="links-right">
+            <a href="#">Products</a>
+            <a href="/../api/login.php">Log in</a>
+            <a href="/../api/logout.php">Log out</a>
+            <a href="/../api/register.php">Register</a>
+            <a href="/../api/bookrequest.php">Bookrequest</a>
+
+
+            <a href="#"><?= isset($user) ? $user['fname'].' '.$user['lname'] : 'Guest' ?></a>
+        </div>
+    </div>
+    <img class="nav-tail" src="/../assets/nav-tail.svg" />
+</nav>
